dd154de3069de5eeee0308040d03fd7ed930650c
Zach Cullen <zacullen@gmail.com>
2020-03-22 09:48:48 -0400

    define create action for new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    authenticated = user.try(:authenticate, params[:password])
+    if authenticated
+      session[:user_id] = authenticated.id
+      redirect_to user_path(authenticated)
+    else
+      redirect_to :login
+      flash[:alert] = "Sorry, invalid email and password combination"
+    end 
   end
 
   def destroy
   end
-  
+
 end
