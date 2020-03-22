9fde4d990bcb6aadd6830bf38fa62ef04aa45ffe
Zach Cullen <zacullen@gmail.com>
2020-03-22 10:35:31 -0400

    create session authenitcations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_action :verify_user
+  helper_method :current_user
+
+  def verify_user
+    redirect_to :root unless user_is_authenticated
+  end
+
+  def user_is_authenticated
+    !!current_user
+  end
+
+  def current_user
+    user ||= User.find_by(id: session[:user_id])
+  end
 end
