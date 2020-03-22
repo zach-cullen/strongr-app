2b5b8849acc2682f7fa648982f4b322c7401d30f
Zach Cullen <zacullen@gmail.com>
2020-03-22 11:30:43 -0400

    generate model Team

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_110422) do
+ActiveRecord::Schema.define(version: 2020_03_22_152749) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
