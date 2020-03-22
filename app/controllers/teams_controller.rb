d17b095c953c4ccb74a5b510982d6c3c320fdccd
Zach Cullen <zacullen@gmail.com>
2020-03-22 11:51:46 -0400

    define create action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,8 +8,21 @@ class TeamsController < ApplicationController
   end
 
   def new
+    @team = Team.new
   end
   
   def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to teams_path
+    else
+      render :new
+    end 
+  end 
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
   end
 end
