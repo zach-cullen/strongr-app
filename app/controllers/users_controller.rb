64f9a4dec8482fc504ddf7fa68d4d04d0010590e
Zach Cullen <zacullen@gmail.com>
2020-03-22 10:38:23 -0400

    protect user content using sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_user, only: [:new, :create]
+
   def new
     @user = User.new
-
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       render :new
@@ -15,6 +17,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   private
