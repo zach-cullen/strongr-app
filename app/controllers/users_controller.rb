4987dd9154bf21f8b03eda3f9798e4261bd2d2d5
Zach Cullen <zacullen@gmail.com>
2020-03-22 06:51:40 -0400

    create user from from

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  
   def new
     @user = User.new
   end
 
   def create
-    raise params.inspect
+    @user = User.new(email: params[:user][:email])
+
   end
 end
