70d6ac81061c99cb64402ca9a2cc0d618271ac3e
Zach Cullen <zacullen@gmail.com>
2020-03-22 11:40:45 -0400

    create teams resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
   root 'pages#home'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:show, :new, :create]
+  resources :teams, only: [:index, :show, :new, :create]
 
   get '/signup', to: 'users#new', as: :signup
   get '/login', to: 'sessions#new', as: :login
