015424013ca814bca12a21c6059d06f3aa4e5b3a
Zach Cullen <zacullen@gmail.com>
2020-03-22 14:12:55 -0400

    add column team_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_152749) do
+ActiveRecord::Schema.define(version: 2020_03_22_180616) do
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_03_22_152749) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "password_digest"
+    t.integer "team_id"
   end
 
 end
