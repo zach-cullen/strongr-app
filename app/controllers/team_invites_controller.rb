34c97c17adfe7477a535bd266cbd34a3c03da607
Zach Cullen <zacullen@gmail.com>
2020-03-23 09:46:50 -0400

    stub controller methods

--- app/controllers/team_invites_controller.rb
+++ app/controllers/team_invites_controller.rb
@@ -1,2 +1,20 @@
 class TeamInvitesController < ApplicationController
+
+  def new
+    #form only for coaches
+  end
+
+  def create
+    #post action only for coaches
+  end
+
+  def show
+    #view for coaches and athletes. Athlete only can accept
+    #link to destroy
+  end
+
+  def destroy
+    #athlete and coach can both destroy
+  end
+
 end
