b819d998f4b22c3b27c9557bb2b38ab657ff66ad
Zach Cullen <zacullen@gmail.com>
2020-03-23 13:44:03 -0400

    define carete action

--- app/controllers/team_invites_controller.rb
+++ app/controllers/team_invites_controller.rb
@@ -1,11 +1,26 @@
 class TeamInvitesController < ApplicationController
 
   def new
-    #form only for coaches
+    #builds invite automatically associated with team
+    @team = current_user.team 
+    @invite = TeamInvite.new
   end
 
   def create
     #post action only for coaches
+    @team = current_user.team
+    if valid_user
+      if !valid_user.has_team?
+        @invite = valid_user.team_invites.build
+        @team.team_invites << @invite
+      else
+        redirect_to new_team_invite_path
+        flash[:alert] = "The user at this address already has a team."
+      end
+    else
+      redirect_to new_team_invite_path
+      flash[:alert] = "No user found with this email."
+    end
   end
 
   def show
@@ -20,6 +35,21 @@ class TeamInvitesController < ApplicationController
     #athlete accepts they mutate self then destroy
     #athlete declines they destroy without mutating self
     #coach can expire invitation to destroy
+  end
+
+  def valid_user
+    @user = User.find_by(email: team_invite_params[:user_attributes][:email])
+    if !!@user
+      @user
+    else
+      false
+    end
+  end
+
+  private
+
+  def team_invite_params
+    params.require(:team_invite).permit(user_attributes: [:email])
   end
 
 end
