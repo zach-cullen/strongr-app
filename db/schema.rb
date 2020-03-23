f149f41f39670b9ad6931cac17c78b4f483ffb07
Zach Cullen <zacullen@gmail.com>
2020-03-23 09:42:45 -0400

    create table team_invites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_181041) do
+ActiveRecord::Schema.define(version: 2020_03_23_133508) do
+
+  create_table "team_invites", force: :cascade do |t|
+    t.integer "team_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
