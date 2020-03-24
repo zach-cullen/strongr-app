664b00c1f63fc61f83cebc6f0779992c4f907f64
Zach Cullen <zacullen@gmail.com>
2020-03-24 16:33:37 -0400

    build association for team to have exercises through children

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,6 @@ class Team < ApplicationRecord
   has_many :users
   has_many :team_invites
   has_many :workouts
+  has_many :workout_exercises, through: :workouts
+  has_many :exercises, through: :workout_exercises
 end
