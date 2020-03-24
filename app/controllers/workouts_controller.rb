29a1a6e56d451bdcaa15f2c47dd0026998e401e6
Zach Cullen <zacullen@gmail.com>
2020-03-24 10:52:29 -0400

    display workout info on show page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,7 @@
 class WorkoutsController < ApplicationController
 
   def show
+    @workout = Workout.find_by(id: params[:id])
   end 
 
   def new
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,7 @@
 class Workout < ApplicationRecord
   belongs_to :team
+
+  def date_pretty
+    self.date.strftime("%B %-d, %Y")
+  end
 end
