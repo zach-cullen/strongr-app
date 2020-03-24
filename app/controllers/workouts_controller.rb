f1c34529316bb02c253525d38f91d9af600e0e9d
Zach Cullen <zacullen@gmail.com>
2020-03-24 17:03:29 -0400

    def workout strong params

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -5,12 +5,20 @@ class WorkoutsController < ApplicationController
   end 
 
   def new
+    @workout = Workout.new
   end
 
   def create
+    raise params.inspect
   end 
 
   def destroy
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).allow(:date)
   end
 
 end
