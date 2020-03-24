e1729c2cefd9577288c3f336ef886b585477c0b2
Zach Cullen <zacullen@gmail.com>
2020-03-24 17:20:48 -0400

    create class method providing array of acceptable categories

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,12 @@ class Exercise < ApplicationRecord
   has_many :workout_exercises
   has_many :workouts, through: :workout_exercises
 
+  def self.categories
+    categories = ["Strength", "Metcon"]
+  end
+
   def rank_by_text
     self.rank_by_max ? "max" : "min"
   end
+
 end
