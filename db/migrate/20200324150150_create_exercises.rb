089a5f43d3d14364d4f874fb937eace38a8d1d04
Zach Cullen <zacullen@gmail.com>
2020-03-24 11:27:02 -0400

    change name of type column to category to avoid error

--- db/migrate/20200324150150_create_exercises.rb
+++ db/migrate/20200324150150_create_exercises.rb
@@ -1,7 +1,7 @@
 class CreateExercises < ActiveRecord::Migration[6.0]
   def change
     create_table :exercises do |t|
-      t.string :type
+      t.string :category
       t.string :title
       t.string :amount_unit
       t.integer :amount
