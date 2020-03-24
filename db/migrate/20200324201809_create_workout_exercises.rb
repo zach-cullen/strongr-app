12ffa88f6102e067fe7bd341cbadbef3f91327eb
Zach Cullen <zacullen@gmail.com>
2020-03-24 16:26:34 -0400

    correct column workout_id

--- db/migrate/20200324201809_create_workout_exercises.rb
+++ db/migrate/20200324201809_create_workout_exercises.rb
@@ -1,7 +1,7 @@
 class CreateWorkoutExercises < ActiveRecord::Migration[6.0]
   def change
     create_table :workout_exercises do |t|
-      t.integer :team_id
+      t.integer :workout_id
       t.integer :exercise_id
 
       t.timestamps
