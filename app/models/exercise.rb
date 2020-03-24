365337a6eacc781be98b6cbac077ac43babe75ce
Zach Cullen <zacullen@gmail.com>
2020-03-24 16:30:59 -0400

    create associations between workouts and exercises through join model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,6 @@
 class Exercise < ApplicationRecord
+  has_many :workout_exercises
+  has_many :workouts, through: :workout_exercises
 
   def rank_by_text
     self.rank_by_max ? "max" : "min"
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,8 @@
 class Workout < ApplicationRecord
   belongs_to :team
+  has_many :workout_exercises
+  has_many :exercises, through: :workout_exercises
+
 
   def date_pretty
     self.date.strftime("%B %-d, %Y")
