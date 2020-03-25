4e5ee03fad3659d5e0732852e8f23e20366169e5
Zach Cullen <zacullen@gmail.com>
2020-03-25 09:48:50 -0400

    format workout_params to accept nested exercise attributes

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -10,7 +10,7 @@ class WorkoutsController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    byebug
   end 
 
   def destroy
@@ -19,23 +19,35 @@ class WorkoutsController < ApplicationController
   private
 
   def workout_params
-    params.require(:workout).allow(:date)
+    params.require(:workout).permit(
+      "date(1i)", "date(2i)", "date(3i)", 
+      :exercise_attributes => [
+        :category,
+        :title,
+        :amount,
+        :amount_unit,
+        :score_unit,
+        :rank_by_max,
+        :description
+      ]
+    ) 
   end
 
   # WORKOUT FORM PARAMS STRUCTURE 
   # "params" => {
   #   "workout"=>{
-  #   "date(1i)"=>"2020", 
-  #   "date(2i)"=>"3",
-  #   "date(3i)"=>"25", 
-  #   "exercise_attributes"=>{
-  #     "category"=>"Strength", 
-  #     "title"=>"Deadlift", 
-  #     "amount"=>"10", 
-  #     "amount_unit"=>"reps", 
-  #     "score_unit"=>"lbs", 
-  #     "rank_by_max"=>"true",
-  #     "description"=>"Build to heavy set rest 2 min"}
+    #   "date(1i)"=>"2020", 
+    #   "date(2i)"=>"3",
+    #   "date(3i)"=>"25",  
+    #   "exercise_attributes"=>{
+    #     "category"=>"Strength", 
+    #     "title"=>"Deadlift", 
+    #     "amount"=>"10", 
+    #     "amount_unit"=>"reps", 
+    #     "score_unit"=>"lbs", 
+    #     "rank_by_max"=>"true",
+    #     "description"=>"Build to heavy set rest 2 min"
+    #   }
   #   } 
   # }
 
