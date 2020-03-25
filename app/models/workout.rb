17461f176dafed24b6258f658d933d540b485ddc
Zach Cullen <zacullen@gmail.com>
2020-03-25 10:36:09 -0400

    define exercise_attributes method to find or build exercise from workout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,6 +3,23 @@ class Workout < ApplicationRecord
   has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
 
+  def exercise_attributes=(attributes)
+    # accepts nested attributes from new workout form for finding or creating new exercise to add to workout
+    exercise = self.team.exercises.find_by(
+      title: attributes[:title],
+      amount: attributes[:amount],
+      amount_unit: attributes[:amount_unit],
+      score_unit: attributes[:score_unit]
+    )
+    # custom find or create to allow description to be updated each time
+    if !!exercise 
+      exercise.update(description: attributes[:description])
+      self.exercises << exercise
+    else
+      self.exercises.build(attributes)
+      self.save
+    end
+  end
 
   def date_pretty
     self.date.strftime("%B %-d, %Y")
