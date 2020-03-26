714da8e7085f1c615924103949148e3b7b169a94
Zach Cullen <zacullen@gmail.com>
2020-03-26 08:57:27 -0400

    accept nested attributes and refactor writer to build multiple exercises

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,22 +2,27 @@ class Workout < ApplicationRecord
   belongs_to :team
   has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
+  accepts_nested_attributes_for :exercises
 
-  def exercise_attributes=(attributes)
+  def exercises_attributes=(attributes)
     # accepts nested attributes from new workout form for finding or creating new exercise to add to workout
-    exercise = self.team.exercises.find_by(
-      title: attributes[:title],
-      amount: attributes[:amount],
-      amount_unit: attributes[:amount_unit],
-      score_unit: attributes[:score_unit]
-    )
-    # custom find or create to allow description to be updated each time
-    if !!exercise 
-      exercise.update(description: attributes[:description])
-      self.exercises << exercise
-    else
-      self.exercises.build(attributes)
-      self.save
+    #iterate over multiple collections of exercise attributes as exercise_hash
+    attributes.values.each do |exercise_hash|
+      # finds if exercise already exists in certain combination of attributes
+      exercise = self.team.exercises.find_by(
+        title: exercise_hash[:title],
+        amount: exercise_hash[:amount],
+        amount_unit: exercise_hash[:amount_unit],
+        score_unit: exercise_hash[:score_unit]
+      )
+      # if exercise already exists, updates descripiton only, else creates a new exercise
+      if !!exercise 
+        exercise.update(description: exercise_hash[:description])
+        self.exercises << exercise
+      else
+        self.exercises.build(exercise_hash)
+        self.save
+      end
     end
   end
 
