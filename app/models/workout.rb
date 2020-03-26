05e12d0944db1e61588aa0ec03691b2b340a5a8f
Zach Cullen <zacullen@gmail.com>
2020-03-26 09:10:02 -0400

    wrap exercise find or create in conditional checking if form section is blank

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -8,20 +8,23 @@ class Workout < ApplicationRecord
     # accepts nested attributes from new workout form for finding or creating new exercise to add to workout
     #iterate over multiple collections of exercise attributes as exercise_hash
     attributes.values.each do |exercise_hash|
-      # finds if exercise already exists in certain combination of attributes
-      exercise = self.team.exercises.find_by(
-        title: exercise_hash[:title],
-        amount: exercise_hash[:amount],
-        amount_unit: exercise_hash[:amount_unit],
-        score_unit: exercise_hash[:score_unit]
-      )
-      # if exercise already exists, updates descripiton only, else creates a new exercise
-      if !!exercise 
-        exercise.update(description: exercise_hash[:description])
-        self.exercises << exercise
-      else
-        self.exercises.build(exercise_hash)
-        self.save
+      #if one of the exercises is left blank skip that section
+      if !exercise_hash[:title].blank?
+        # finds if exercise already exists in certain combination of attributes
+        exercise = self.team.exercises.find_by(
+          title: exercise_hash[:title],
+          amount: exercise_hash[:amount],
+          amount_unit: exercise_hash[:amount_unit],
+          score_unit: exercise_hash[:score_unit]
+        )
+        # if exercise already exists, updates descripiton only, else creates a new exercise
+        if !!exercise 
+          exercise.update(description: exercise_hash[:description])
+          self.exercises << exercise
+        else
+          self.exercises.build(exercise_hash)
+          self.save
+        end
       end
     end
   end
