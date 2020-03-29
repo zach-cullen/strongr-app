95e258ea3137a1afd1bc4fc4ef38901d995b0cb8
Zach Cullen <zacullen@gmail.com>
2020-03-29 17:55:11 -0400

    remove associations with destroyed models

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,8 +3,6 @@ class Team < ApplicationRecord
   has_many :users
   has_many :team_invites
   has_many :workouts
-  has_many :workout_exercises, through: :workouts
-  has_many :exercises, through: :workout_exercises
 
   def exercises_list
     self.exercises.order(:title).select(:title).distinct
