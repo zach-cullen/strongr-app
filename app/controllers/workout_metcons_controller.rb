96d3d09739330f9db5c29628b926e6a5ad96d87e
Zach Cullen <zacullen@gmail.com>
2020-03-30 11:54:32 -0400

    define destroy action for workout_metcons join

--- app/controllers/workout_metcons_controller.rb
+++ app/controllers/workout_metcons_controller.rb
@@ -1,2 +1,22 @@
 class WorkoutMetconsController < ApplicationController
+  before_action :permit_coach
+
+  def destroy
+    @workout_metcon = WorkoutMetcon.find_by(id: params[:id])
+    #cache workout prior to deleting join for validation and redirect
+    @workout = @workout_metcon.workout
+    if @workout.team == current_user.team
+      #deletes join model and redirects to page rendering the original workout
+      @workout_metcon.destroy
+      redirect_to workout_path(@workout)
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def permit_coach
+    if !current_user.is_coach?
+      redirect_to user_path(current_user)
+    end
+  end
 end
