824e372f32cecc045239594a2f443a4fdf86406f
Zach Cullen <zacullen@gmail.com>
2020-03-30 15:47:36 -0400

    move valid editor to helper method

--- app/controllers/metcons_controller.rb
+++ app/controllers/metcons_controller.rb
@@ -3,20 +3,27 @@ class MetconsController < ApplicationController
 
   def edit
     @metcon = Metcon.find_by(id: params[:id])
-    valid_editor = current_user.team.metcons.include?(@metcon)
+    if !valid_editor
+      redirect_to user_path(current_user)
+    end
+    byebug
   end
 
   def update
     @metcon = Metcon.find_by(id: params[:id])
-    valid_editor = current_user.team.metcons.include?(@metcon)
     if valid_editor
       @metcon.update(metcon_params)
+      redirect_to team_path(current_user.team)
     else
       redirect_to user_path(current_user)
     end
   end
 
   private
+
+  def valid_editor
+    current_user.team.metcons.include?(@metcon)
+  end
 
   def metcon_params
     params.require(:metcon).permit(:description, :score_by)
