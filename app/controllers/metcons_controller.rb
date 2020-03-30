b39df13b055fc22b60db77200dcc49e80e7af056
Zach Cullen <zacullen@gmail.com>
2020-03-30 15:14:02 -0400

    define edit and update actions

--- app/controllers/metcons_controller.rb
+++ app/controllers/metcons_controller.rb
@@ -1,10 +1,19 @@
 class MetconsController < ApplicationController
+  before_action :permit_coach
 
   def edit
-    
+    @metcon = Metcon.find_by(id: params[:id])
+    valid_editor = current_user.team.metcons.include?(@metcon)
   end
 
   def update
-
+    @metcon = Metcon.find_by(id: params[:id])
+    valid_editor = current_user.team.metcons.include?(@metcon)
+    if valid_editor
+      @metcon.update(metcon_params)
+    else
+      redirect_to user_path(current_user)
+    end
   end
+
 end
