6d9df99ae1ec95763a9c36280c5a44f3d7ab001c
Zach Cullen <zacullen@gmail.com>
2020-03-30 09:31:44 -0400

    define method for returning alphabetical list of distinct metcon names

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,4 +6,8 @@ class Team < ApplicationRecord
   has_many :workout_metcons, through: :workouts
   has_many :metcons, through: :workout_metcons
 
+  def metcons_list
+    #returns an alphabetically ordered array of distinct metcon titles specific to the team
+    metcons_titles = self.metcons.order(:title).select(:title).distinct.collect {|m| m.title}
+  end
 end
