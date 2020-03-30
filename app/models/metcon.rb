07d1d2f576507227022a543582f507e0a69393d9
Zach Cullen <zacullen@gmail.com>
2020-03-30 09:44:18 -0400

    validate score_type string with array of valid options

--- app/models/metcon.rb
+++ app/models/metcon.rb
@@ -1,6 +1,7 @@
 class Metcon < ApplicationRecord
   has_many :workout_metcons
   has_many :workouts, through: :workout_metcons
+  validates :score_by, inclusion: { in: ["time", "reps"] }
   
   def self.score_types
     #provides valid score_types to form
