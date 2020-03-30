2cee9a855e06821740e286836c6104e777ce7c50
Zach Cullen <zacullen@gmail.com>
2020-03-30 09:10:57 -0400

    create class method to provide valid score types

--- app/models/metcon.rb
+++ app/models/metcon.rb
@@ -2,4 +2,8 @@ class Metcon < ApplicationRecord
   has_many :workout_metcons
   has_many :workouts, through: :workout_metcons
   
+  def self.score_types
+    #provides valid score_types to form
+    score_types = ["time", "reps"]
+  end
 end
