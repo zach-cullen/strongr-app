8f2e53f23a2224d58bf73bc196c37f2967fb1c9d
Zach Cullen <zacullen@gmail.com>
2020-03-29 20:07:27 -0400

    build macro association to team metcons

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,5 +3,7 @@ class Team < ApplicationRecord
   has_many :users
   has_many :team_invites
   has_many :workouts
+  has_many :workout_metcons, through: :workouts
+  has_many :metcons, through: :workout_metcons
 
 end
