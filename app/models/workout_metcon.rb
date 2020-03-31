a62bdb256eacfffb9de5aace0c355e837ae9b335
Zach Cullen <zacullen@gmail.com>
2020-03-31 12:05:06 -0400

    def method find_or_build_user_score

--- app/models/workout_metcon.rb
+++ app/models/workout_metcon.rb
@@ -2,4 +2,14 @@ class WorkoutMetcon < ApplicationRecord
   belongs_to :workout
   belongs_to :metcon 
   has_many :workout_metcon_scores
+  accepts_nested_attributes_for :workout_metcon_scores
+
+  def find_or_build_user_score(user)
+    score = workout_metcon_scores.find_by(user_id: user.id)
+    if !!score
+      score
+    else 
+      self.workout_metcon_scores.build(user_id: user.id)
+    end
+  end
 end
