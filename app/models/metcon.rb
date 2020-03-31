4e3e309e409a1c96458713ce25bc67f41509193e
Zach Cullen <zacullen@gmail.com>
2020-03-31 09:11:13 -0400

    add nested workout_id method

--- app/models/metcon.rb
+++ app/models/metcon.rb
@@ -1,8 +1,13 @@
 class Metcon < ApplicationRecord
   has_many :workout_metcons
   has_many :workouts, through: :workout_metcons
+  accepts_nested_attributes_for :workouts
   validates :score_by, inclusion: { in: ["time", "reps"] }
-  
+
+  def workout_id=(attributes)
+    #pass through and do nothing, see metcon controller create with workout_id
+  end
+
   def self.score_types
     #provides valid score_types to form
     score_types = ["time", "reps"]
