53969d1c2751213c56c02d3539311b8f67e98689
Zach Cullen <zacullen@gmail.com>
2020-04-03 09:01:29 -0400

    add route for app/home

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :metcons, only: [:create, :edit, :update]
   resources :workout_metcons, only: [:update]
 
+  get '/app/home', to: 'app#home', as: :app_home
   get '/auth/google_oauth2/callback', to: 'sessions#google_login'
   get '/signup', to: 'users#new', as: :signup
   get '/login', to: 'sessions#new', as: :login
