27a69ecb97a2285a27825d7f111dd1605e6d1f8f
Zach Cullen <zacullen@gmail.com>
2020-04-04 15:14:59 -0400

    define wod action and add team validation to prevent erros

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,6 +1,22 @@
 class AppController < ApplicationController
+  before_action :validate_has_team
+
   def home
     @team = current_user.team
     @workouts_this_week = @team.workouts.this_week
+  end
+
+  def wod
+    @team = current_user.team
+    @workout = @team.workout_of_the_day
+    if @workout
+      redirect_to workout_path(@workout)
+    else
+      redirect_to 'app/home'
+    end
+  end
+
+  def validate_has_team
+    redirect_to user_path(current_user) if !current_user.has_team?
   end
 end
