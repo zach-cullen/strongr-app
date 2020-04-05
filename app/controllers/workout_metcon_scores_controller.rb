4dc18a705ad05e845a403a61926fcddd5d196995
Zach Cullen <zacullen@gmail.com>
2020-04-05 17:44:32 -0400

    define strong params for scores

--- app/controllers/workout_metcon_scores_controller.rb
+++ app/controllers/workout_metcon_scores_controller.rb
@@ -1,2 +1,13 @@
 class WorkoutMetconScoresController < ApplicationController
+
+  def create
+    
+    byebug
+  end
+
+  private
+
+  def score_params
+    params.require(:workout_metcon_score).permit(:score, :min, :sec)
+  end
 end
