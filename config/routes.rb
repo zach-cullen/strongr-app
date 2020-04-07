097b2bea45f63e7344618aa64fefcd3450474461
Zach Cullen <zacullen@gmail.com>
2020-04-07 15:45:32 -0400

    build nested resource team workouts new and create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
   root 'pages#home'
   resources :users, only: [:show, :new, :create]
-  resources :teams, only: [:index, :show, :new, :create]
+  resources :teams, only: [:index, :show, :new, :create] do 
+    resources :workouts, only: [:new, :create]
+  end
+    
   resources :team_invites, only: [:show, :new, :create, :destroy]
   resources :workouts, only: [:show, :new, :create, :edit, :update]
   resources :metcons, only: [:create, :edit, :update]
